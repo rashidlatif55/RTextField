126fc497980af985c44f03ecd8d9a002e87a3927
Rashid Latif <Rashid@vaultspay.ae>
2022-02-07 18:01:49 +0400

    First commit

--- RTextField/ViewController.swift
+++ RTextField/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet private weak var emailTextField:RTextField!
+    @IBOutlet private weak var passwordTextField:RTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func showErrors(_ sender: Any) {
+        self.emailTextField.setError(errorString: "Incorrect E-mail")
+        self.passwordTextField.setError(errorString: "Incorrect Password")
+        print(passwordTextField.frame.width)
     }
 
-
 }
-
